b68a2cd464861e4b58b4b5a1c1da57f391d55d49
krslakdan <191279454+krslakdan@users.noreply.github.com>
2025-10-04 13:38:54 +0200

    Add function to generate logic for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3) + 1;
+  if (choice == 1) {
+    console.log("Rock");
+  } else if (choice == 2) {
+    console.log("Paper");
+  } else console.log("Scissors");
+}
+
+getComputerChoice();
